10c64f09074eda3b272bdf6a74936c95c0adb221
teslatronic <daanrijks@gmail.com>
2014-06-08 18:41:02 +0200

    Changed to __name__ == '__main__' idiom
    Also added some whitespace

--- resizr.py
+++ resizr.py
@@ -14,30 +14,18 @@ subreddits = config['General']['subreddits'].split(';')
 
 titleRegex = re.compile(r"([0-9]+) ?[xX] ?([0-9]+)")
 
-reddit = praw.Reddit(user_agent=USER_AGENT)
-reddit.login(USER, PASS)
-print("Sucessfully logged in")
-
-already_done = []
-
-while True:
-	for subreddit in subreddits:
-		curSub = reddit.get_subreddit(sub) # Get the subreddit object for the current subreddit
-		newSubmissions = curSub.get_new(limit=20)
-		for submission in newSubmissions:
-			if submission.id not in already_done and matchesTitle(submission):
-				
-				reply = assembleReply(submission)
-		
+	
 def matchesTitle(submission): # Uses regex to check if the title matches the pattern
 	match = titleRegex.search(submission.title)
 	if match:
 		return True
+
 		
 def parseSize(submission): # Gets the X and Y sizes from the request using the groups defined in the regex
 	match = titleRegex.search(submission.title)
 	return (int(match.group(1)), int(match.group(2)))
-	
+
+
 def assembleReply(submission): # Assemble the reply, i.e. do all the image magic and put it in a nicely formatted comment
 	requestSize = parseSize(submission)
 	
@@ -47,3 +35,19 @@ def assembleReply(submission): # Assemble the reply, i.e. do all the image magic
 			"I've tried my best to create some resizes. Here they are:"
 	
 	return reply
+
+
+if __name__ == '__main__':
+	reddit = praw.Reddit(user_agent=USER_AGENT)
+	reddit.login(USER, PASS)
+	print("Sucessfully logged in")
+
+	already_done = []
+	while True:
+		for subreddit in subreddits:
+			curSub = reddit.get_subreddit(sub) # Get the subreddit object for the current subreddit
+			newSubmissions = curSub.get_new(limit=20)
+			for submission in newSubmissions:
+				if submission.id not in already_done and matchesTitle(submission):
+					
+					reply = assembleReply(submission)
