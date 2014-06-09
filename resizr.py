368bf244a2ca8149aa7ffdfeb70f17999d1d80b0
teslatronic <daanrijks@gmail.com>
2014-06-09 20:22:57 +0200

    Initial work on Google img search
    Doesn't work yet of course, but I wanna move on to other things now. :p

--- resizr.py
+++ resizr.py
@@ -26,6 +26,8 @@ TITLE_REGEX = re.compile(r"([0-9]+) ?[xX] ?([0-9]+)")  # Regular expression for 
 #Imgur API data
 IMGUR_BASE = "http://api.imgur.com/3/"
 
+#Google API data
+GOOGLE_SBI_UPLOAD = "http://www.google.com/searchbyimage/upload"
 
 
 def matches_title(submission):
@@ -42,14 +44,35 @@ def parse_size(submission):
 
 
 def download_image(submission):
-    """Download the image a submission links to and store it in tmp, named after the submission ID"""
+    """
+    Download the image a submission links to and store it in tmp, named after the submission ID
+    Also returns the binary contents of the image.
+    """
 
     for extension in IMAGE_EXTENSIONS:
         if submission.url.endswith(extension):
             res = requests.get(submission.url)
-            with open('tmp/{}.jpg'.format(submission.id), mode='wb') as img:
+            with open('tmp/{}.jpg'.format(submission.id), mode='wb+') as img:
                 img.write(res.content)
+                img.seek(0)
+                return img.read()
 
+
+def google_image_search_link(submission):
+    """
+    Takes binary image data, and POSTs this to googles image search server.
+    Google's server then generates base64 data for the image search algorithm.
+    This function then returns the resulting query url using this data.
+    More info: http://stackoverflow.com/questions/7584808/google-image-search-how-do-i-construct-a-reverse-image-search-url
+    Also: http://www.rankpanel.com/blog/google-search-parameters/
+    """
+    with open('tmp/{}.jpg'.format(submission.id), mode='rb') as img:
+        payload = {
+            'encoded_image': img,
+        }
+
+    res = requests.post(GOOGLE_SBI_UPLOAD, file=payload)
+    return res.headers['location']
 
 
 def upload_image(image):
@@ -90,10 +113,13 @@ if __name__ == '__main__':
                     already_done_string = already_done.read()
 
                     if submission.id not in already_done_string and matches_title(submission):
-                        print("Match found! id:{} url:{}".format(submission.id, submission.short_link))
+                        print("Match found! url:{}".format(submission.id, submission.short_link))
 
                         download_image(submission)
                         print("Image downloaded!")
+
+                        google_link = google_image_search_link(submission)
+                        print()
 
                         reply = "This matches! Yay!"
                         submission.add_comment(reply)
